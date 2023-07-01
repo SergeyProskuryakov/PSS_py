10825a43f4eff1d6c643f3ab0e6b5de95904f67d
SergeyProskuryakov <130496878+SergeyProskuryakov@users.noreply.github.com>
2023-07-01 12:07:20 +0300

    расчёт отпуска по имени сотрудника

--- 28_06/DZ_28_06_2.py
+++ 28_06/DZ_28_06_2.py
@@ -1,2 +1,18 @@
 # Усложнение: человек вводит своё имя.
 # В "базе данных" хранится для каждого человека количество взятых дней отпуска. {'Вася': 42}
+
+sotrudnyki = {
+    'Катя': 30,
+    'Вася': 42,
+    'Маша': 24,
+    'Петя': 15
+}
+name_sotrudnika = input('Введите имя сотрудника (Катя, Вася, Маша, Петя): ')
+first_month = int(input('Первый месяц (1-12): '))
+second_month = int(input('Последний месяц (1-12): '))
+day = (second_month - first_month) * 2   # если изначально у него стандартный отпуск, то +28 (или сколько там он должен быть)
+
+if name_sotrudnika:
+    sotrudnyki[name_sotrudnika] -= day
+
+print('Дни отпуска сотрудника', name_sotrudnika, ':', sotrudnyki[name_sotrudnika])
