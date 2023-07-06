81601650196ae95cf39560fce9d5d4287f1f68e6
SergeyProskuryakov <130496878+SergeyProskuryakov@users.noreply.github.com>
2023-07-06 20:45:58 +0300

    переименование папок

--- 6_7/exceptions_1.py
+++
@@ -1,7 +0,0 @@
-znam = int(input('Введите число, я найду обратное:'))
-try:
-    result = 1 / znam
-except ZeroDivisionError:
-    result = None
-if result:
-    print('Обратное равно ', result)
---
+++ while/while_3.py
@@ -0,0 +1,31 @@
+# 1. Перепишите программу, избежав "continue"
+# 2. Посчитайте количество верных и неверных ответов, процент ошибок
+# 3. ГОРШОЧЕК, НЕ ВАРИ! Поставьте ограничения:
+#     a. По количеству заданий
+#     b. По максимуму ошибок
+#     c. По минимальному проценту ошибок
+
+from random import randint
+a = randint(1, 10)
+b = randint(1, 10)
+answer = 1
+result = 0
+while answer != result:
+    result = a + b
+    answer = int(input('Сколько будет %(a)i + %(b)i = ' % {'a': a, 'b': b }))
+    print('Не правильно, попробуй ещё раз ')
+print('Верно')
+
+
+
+
+# while True:
+#     result = a + b
+#     answer = int(input('Сколько будет %(a)i + %(b)i = ' % {
+#         'a': a, 'b': b }))
+#     if answer != result:
+#         print('Не правильно, попробуй ещё раз ')
+#         continue
+#     print('Верно')
+#     a = randint(1, 10)
+#     b = randint(1, 10)
