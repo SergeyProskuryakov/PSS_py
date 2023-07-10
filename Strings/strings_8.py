cf1721f7a6538b2f4cd5c3eed8a82dd6ac6117f8
SergeyProskuryakov <130496878+SergeyProskuryakov@users.noreply.github.com>
2023-07-10 20:29:30 +0300

    строки 8 задача

---
+++ Strings/strings_8.py
@@ -0,0 +1,5 @@
+# С клавиатуры вводится строка. Сколько в ней символов? Вводится число. Сколько в нем цифр?
+stroka = input('stroka: ')
+print(len(stroka))
+chislo = int(input('chislo: '))
+print(len(chislo)) # выдаёт ошибку
