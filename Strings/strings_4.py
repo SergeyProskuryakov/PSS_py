2a7f652508cae07f2966c54641bb4e400a1218da
SergeyProskuryakov <130496878+SergeyProskuryakov@users.noreply.github.com>
2023-07-10 19:55:22 +0300

    строки 4 задачка

---
+++ Strings/strings_4.py
@@ -0,0 +1,11 @@
+snachala = 'Было слово '
+potom = 'получился текст'
+
+sohranim = snachala
+snachala += potom
+
+# Каковы значения переменных? 
+# Что случилось с переменной "snachala"?
+
+print(sohranim)
+print(snachala) 
